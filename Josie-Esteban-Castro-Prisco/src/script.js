f7821d666c0817df89ab85656ea78eb6789e908f
JosieEsteban <lkuro123@gmail.com>
2025-11-13 01:59:03 -0500

    Se realizo el Punto 1: Puntos Dobles del taller 2

---
+++ Josie-Esteban-Castro-Prisco/src/script.js
@@ -0,0 +1,28 @@
+
+// Taller
+
+  /* Punto 1. 
+  
+  Tareas:
+
+  1.  Crea un nuevo arreglo `puntosDoble` donde cada valor sea el doble del arreglo original.
+  2.  Muestra `puntos` y `puntosDoble` para verificar que el original no cambió. */
+
+ let puntos = [10, 20, 30, 40];
+
+ let puntosDoble = puntos.map((num) => {
+
+      return num * 2;      
+ }) 
+
+ console.log(puntos);
+ console.log(puntosDoble);
+
+ //*******************************************
+ 
+
+
+
+
+
+
